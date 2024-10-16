36ed5b0e5df7be70ad363cb5955acbd1ad1d25c2
ryu <fbqudrlf09@naver.com>
2024-10-16 10:58:59 +0900

    필수 기능 가이드 3까지 완료

--- src/com/example/calculator/Calculator.java
+++ src/com/example/calculator/Calculator.java
@@ -1,8 +1,47 @@
 package com.example.calculator;
 
+import java.util.Scanner;
+
 public class Calculator {
 
     public static void main(String[] args) {
-        System.out.println("Hello Calculator");
+
+        Scanner sc = new Scanner(System.in);
+
+        int firstNum = sc.nextInt();
+        int secondNum = sc.nextInt();
+
+        sc.nextLine();
+        char operator = sc.nextLine().charAt(0);
+        int result = 0;
+        boolean flag = true;
+
+        switch (operator) {
+            case '+':{
+                result = firstNum + secondNum;
+                break;
+            }
+            case '-':{
+                result = firstNum - secondNum;
+                break;
+            }
+            case '*':{
+                result = firstNum * secondNum;
+                break;
+            }
+            case '/':{
+                if(secondNum == 0){
+                    System.out.println("분모가 0입니다 나누기 계산이 되지 않습니다.");
+                    flag = false;
+                    break;
+                }
+                result = firstNum / secondNum;
+                break;
+            }
+        }
+
+        if(flag == true)
+            System.out.println("result = " + result);
     }
+
 }
